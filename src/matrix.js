08542a86871673831e7f0cbcb8a14fba365523e4
Wesley Unwin <wes_unwin@hotmail.com>
2016-09-27 22:22:57 -0400

    Implement matrix-matrix multiply and write test

--- src/matrix.js
+++ src/matrix.js
@@ -2,24 +2,48 @@ var Matrix = function(values) {
 
   this.values = values;
 
-  this.loadIdentity = function() {
-    this.values = [1, 0, 0, 0,
-                   0, 1, 0, 0,
-                   0, 0, 1, 0,
-                   0, 0, 0, 1];
-  };
 };
-  // function loadIdentity() {
-  //   mvMatrix = Matrix.I(4);
-  // }
 
-  // function multMatrix(m) {
-  //   mvMatrix = mvMatrix.x(m);
-  // }
+Matrix.prototype.loadIdentity = function() {
+  this.values = [1, 0, 0, 0,
+                 0, 1, 0, 0,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1];
+};
 
-  // function mvTranslate(v) {
-  //   multMatrix(Matrix.Translation($V([v[0], v[1], v[2]])).ensure4x4());
-  // }
+Matrix.prototype.transformVector = function(vector) {
+
+
+
+};
+
+Matrix.prototype.multiply = function(matrix) {
+  var result = [0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0];
+  var a = this.values;
+  var b = matrix;
+  var s1, s2, s3, s4;
+  for(var c = 0; c < 4; c++) // column in result matrix
+    for(var r = 0; r < 4; r++) { // row in result matrix
+      s1 = a[r*4] * b[c];
+      s2 = a[r*4 + 1] * b[c + 4];
+      s3 = a[r*4 + 2] * b[c + 8];
+      s4 = a[r*4 + 3] * b[c + 12];
+      result[c + (r*4)] = s1 + s2 + s3 + s4;
+    }
+  this.values = result;
+};
+
+Matrix.prototype.translate = function(x, y, z) {
+  this.values[12] += x;
+  this.values[13] += y;
+  this.values[14] += z;
+};
+
+Matrix.prototype.rotate = function(degrees, x,y,z) {
+
+
+
+};
 
   // function setMatrixUniforms() {
   //   var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
--- tests/MatrixSpec.js
+++ tests/MatrixSpec.js
@@ -12,6 +12,24 @@ describe('Matrix', function() {
 
   });
 
+  describe('multiply', function() {
+
+    it('multiplies itself (the left matrix) by the given matrix (the right matrix)', function() {
+      var m = new Matrix([2,2,1,3, 1,2,-1,3, 1,4,1,2, -1,2,1,3]);
+      m.multiply([0,3,2,0, 1,4,2,3, -3,1,2,1, 1,3,2,2]);
+      expect(m.values).toEqual([2,24,16,13, 8,19,10,11, 3,26,16,17, 2,15,10,13]);
+    });
+
+  });
+
+  describe('translate', function() {
+    
+    it('multiples the matrix by a translation matrix that shifts points by x,y,z units', function() {
+
+    });
+
+  });
+
   it('passes', function() {
     console.log("test run");
     //expect(1).toBe(2);
