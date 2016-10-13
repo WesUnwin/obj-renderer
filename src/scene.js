10e091bc585448bc1592c65853f34606d3e606c5
Wesley Unwin <wes_unwin@hotmail.com>
2016-10-12 21:43:30 -0400

    Supply vertex color info - use VBO for color attribute, pass through shaders

--- src/scene.js
+++ src/scene.js
@@ -1,4 +1,5 @@
 var Matrix = require('./matrix.js');
+var ModelStaticVBO = require('./graphics/ModelStaticVBO.js');
 
 class Scene {
 
@@ -8,19 +9,8 @@ class Scene {
     this.projectionMatrix = new Matrix();
 
     this.modelViewMatrix = new Matrix([1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0.0,1]);
-        //mvTranslate([-0.0, 0.0, -6.0]);
 
-    this.squareVerticesBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.squareVerticesBuffer);
-
-    var vertices = [
-         0.0,  0.5,  0.0,
-        -0.5, -0.5,  0.0,
-         0.5, -0.5,  0.0
-    ];
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
+    this.objects.push(new ModelStaticVBO('a'));
   }
 
   addObject(object) {
@@ -33,12 +23,7 @@ class Scene {
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.squareVerticesBuffer);
 
-    var vertexPositionAttribute = gl.getAttribLocation(shaderProgram, 'aVertexPosition');
-    gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
-
     // Set Perspective Matrix (a uniform)
-    //var perspectiveMatrix = makePerspective(45, 640.0/480.0, 0.1, 100.0);
-
     var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
     if (!pUniform) throw "Could not get location of uPMatrix";
     gl.uniformMatrix4fv(pUniform, false, new Float32Array(this.projectionMatrix.values));
@@ -48,11 +33,8 @@ class Scene {
     if (!mvUniform) throw "Could not get location of mvUniform";
     gl.uniformMatrix4fv(mvUniform, false, new Float32Array(this.modelViewMatrix.values));
 
-
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
-
     for(var i = 0; i < this.objects.length; i++) {
-      this.objects[i].render();
+      this.objects[i].render(gl, shaderProgram);
     }
   }
 
