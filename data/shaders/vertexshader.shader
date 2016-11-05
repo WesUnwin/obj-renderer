06046876471726c1eaf5e9397bfb0bc1072eaf42
Wesley Unwin <wes_unwin@hotmail.com>
2016-11-04 21:56:13 -0400

    Get shader source from .shader files (via stringify transform)

---
+++ data/shaders/vertexshader.shader
@@ -0,0 +1,16 @@
+attribute vec3 aVertexPosition;
+attribute vec4 aVertexColor;
+attribute vec3 aVertexTextureCoords;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+
+varying vec4 vColor;
+varying vec3 vTextureCoords;
+
+void main(void) {
+  vColor = aVertexColor;
+  vTextureCoords = aVertexTextureCoords;
+
+  gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+}
--- src/shaders.js
+++ src/shaders.js
@@ -1,20 +1,21 @@
+let DefaultVertexShaderSource = require('../data/shaders/vertexshader.shader');
+let DefaultFragmentShaderSource = require('../data/shaders/fragmentshader.shader');
+
 module.exports = {
 
   setupShaders: function(gl) {
     var shaderProgram;
 
-    var vertexShaderScriptElement = document.getElementById('shader-vs');
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShader, vertexShaderScriptElement.text);
+    gl.shaderSource(vertexShader, DefaultVertexShaderSource);
     gl.compileShader(vertexShader);         
     if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
       alert('Vertex shader did not compile');
       return;
     }
 
-    var fragmentShaderScriptElement = document.getElementById('shader-fs');
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShader, fragmentShaderScriptElement.text);
+    gl.shaderSource(fragmentShader, DefaultFragmentShaderSource);
     gl.compileShader(fragmentShader);           
     if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
       alert('Fragment shader did not compile');
