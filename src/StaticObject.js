696674ebe6e61d5e769e638370112beab777bd5e
Wesley Unwin <wes_unwin@hotmail.com>
2017-07-15 22:50:17 -0400

    Create SceneObject super class

--- src/StaticObject.js
+++ src/StaticObject.js
@@ -1,71 +1,15 @@
+'use strict';
+
 const ModelStaticVBO = require('../src/graphics/ModelStaticVBO.js');
 const Matrix = require('./Matrix.js');
+const SceneObject = require('./SceneObject.js');
 
 
-class StaticObject {
+class StaticObject extends SceneObject {
 
   constructor(model) {
+    super();
 		this.modelStaticVBO = new ModelStaticVBO(model);
-
-    this.x = 0;
-    this.y = 0;
-    this.z = 0;
-    this.sx = 1;
-    this.sy = 1;
-    this.sz = 1;
-    this.pitch = 0; // in degrees
-    this.yaw = 0;
-
-	  this.transform = new Matrix(); // needs to be kept up to date with above values
-    this.subObjects = [];
-  }
-
-  resetTransform() {
-    this.transform.loadIdentity();
-  }
-
-  setPosition(x,y,z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-    this._updateTransform();
-  }
-
-  setScale(sx, sy, sz) {
-    this.sx = sx;
-    this.sy = sy;
-    this.sz = sz;
-    this._updateTransform();
-  }
-
-  setPitch(degrees) {
-    this.pitch = degrees;
-    this._updateTransform();
-  }
-
-  setYaw(degrees) {
-    this.yaw = degrees;
-    this._updateTransform();
-  }
-
-  _updateTransform() {
-    this.transform.loadIdentity();
-    this.transform.rotate(this.yaw, 1,0,0);
-    this.transform.rotate(this.pitch, 0,1,0);
-    this.transform.scale(this.sx, this.sy, this.sz);
-    this.transform.setTranslation(this.x, this.y, this.z);
-  }
-
-  rotate(degrees, x, y, z) {
-    this.transform.rotate(degrees, x, y, z);
-  }
-
-  scale(sx, sy, sz) {
-    this.transform.scale(sx, sy, sz);
-  }
-
-  addObject(object) {
-    this.subObjects.push(object);
   }
 
   render(gl, projectionMatrix, modelViewMatrix) {
