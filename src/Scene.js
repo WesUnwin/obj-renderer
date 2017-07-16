e50be009822af1cb9ad5610e5eac5463b06940fe
Wesley Unwin <wes_unwin@hotmail.com>
2017-07-16 17:49:35 -0400

    Improve Scene initialization of GL rendering settings

--- src/Scene.js
+++ src/Scene.js
@@ -20,11 +20,11 @@ class Scene {
     this.setViewPort(viewportX, viewportY, viewportWidth, viewportHeight);
 
     this.camera = new Camera();
-
     this.objects = [];
-
     window.defaultShaderProgram = new ShaderProgram(this.gl, DefaultVertexShaderSource, DefaultFragmentShaderSource);
     window.texturedShaderProgram = new ShaderProgram(this.gl, TexturedVertexShaderSource, TexturedFragmentShaderSource);
+
+    this._setRenderingDefaults();
   }
 
   setViewPort(x = 0, y = 0, width = this.canvas.width, height = this.canvas.height) {
@@ -43,6 +43,12 @@ class Scene {
     });
   }
 
+  setBackDropColor(red, green, blue) {
+    const gl = this.gl;
+    // Set values to clear framebuffer bits to:
+    gl.clearColor(red, green, blue, 1.0);  // Clear to black, fully opaque
+  }
+
   enableBackFaceCulling(cullBackFaces = true) {
     const gl = this.gl;
     if (cullBackFaces) {
@@ -54,19 +60,21 @@ class Scene {
     }
   }
 
+  enableDepthTest(useDepthTesting = true) {
+    const gl = this.gl;
+    if (useDepthTesting) {
+      gl.enable(gl.DEPTH_TEST);  // Enable depth testing
+      gl.depthFunc(gl.LESS);     // Draw pixels with a Z value less than the z value of the pixel already drawn at the same location on the frame buffer
+      gl.depthMask(true);        // allow writing to Z-buffer
+    } else {
+      gl.disable(gl.DEPTH_TEST);
+    }
+  }
+
   render() {
     const gl = this.gl;
 
-    // Depth Testing
-    gl.enable(gl.DEPTH_TEST);  // Enable depth testing
-    gl.depthFunc(gl.LESS);     // Draw pixels with a Z value less than the z value of the pixel already drawn at the same location on the frame buffer
-    gl.depthMask(true);        // allow writing to Z-buffer
-
-    // Set values to clear framebuffer bits to:
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-    gl.clearDepth(1.0);                 // Clear everything
-
-    // Clear the framebuffer
+    // Clear the framebuffer bits:  (to the currently set clearColor and clearDepth values)
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     gl.bindBuffer(gl.ARRAY_BUFFER, this.squareVerticesBuffer);
@@ -83,6 +91,15 @@ class Scene {
     setInterval(() => {
       this.render(this.gl); 
     }, 1500);
+  }
+
+  _setRenderingDefaults() {
+    this.gl.clearDepth(1.0);  // Sets the value to clear the depth buffer to when using gl.clear() 
+                         // (does not actual clear the buffer)
+
+    this.setBackDropColor(0, 0, 0);
+    this.enableBackFaceCulling(false);
+    this.enableDepthTest(true);
   }
 
 }
