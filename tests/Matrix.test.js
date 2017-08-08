afa1a955ae6b930cac9ea6b07f732d040047cdd3
Wesley Unwin <wes_unwin@hotmail.com>
2017-08-07 21:25:52 -0400

    Setup up jest

--- tests/Matrix.test.js
+++ tests/Matrix.test.js
@@ -1,4 +1,7 @@
-var Matrix = require('../src/matrix.js');
+jest.dontMock('../src/Matrix.js');
+
+
+var Matrix = require('../src/Matrix.js');
 
 describe('Matrix', () => {
 
@@ -40,7 +43,7 @@ describe('Matrix', () => {
 
     it('multiplies itself (the left matrix) by the given matrix (the right matrix)', () => {
       var m = new Matrix([2,2,1,3, 1,2,-1,3, 1,4,1,2, -1,2,1,3]);
-      m.multiply([0,3,2,0, 1,4,2,3, -3,1,2,1, 1,3,2,2]);
+      m.multiply(new Matrix([0,3,2,0, 1,4,2,3, -3,1,2,1, 1,3,2,2]));
       expect(m.values).toEqual([2,24,16,13, 8,19,10,11, 3,26,16,17, 2,15,10,13]);
     });
 
